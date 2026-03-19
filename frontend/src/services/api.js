5f4ef6c46b19fac9e99b8bd298e5addd7e86f81a
Harini <harinidanalyst@gmail.com>
2026-03-19 17:49:31 +0530

    fix API URL to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://multilingual-ai-dataanalyst.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
@@ -24,4 +24,5 @@ API.interceptors.response.use(
 );
 
 export default API;
+
 
